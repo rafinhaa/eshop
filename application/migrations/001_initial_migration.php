65130091da60e49834ddd5827c5609b915c74ce9
Rafael Rodrigues <rafinhaa@me.com>
2021-03-17 17:42:46 -0300

    Update 001_initial_migration.php

--- application/migrations/001_initial_migration.php
+++ application/migrations/001_initial_migration.php
@@ -344,12 +344,173 @@ class Migration_Initial_migration extends CI_Migration {
         'forma_envio' => array(
                 'type' => 'TINYINT',
                 'constraint' => 1
-        )
+        ),
+        'CONSTRAINT pedidos_clientes_id_fk FOREIGN KEY (id_cliente) REFERENCES clientes (id) ON DELETE SET NULL'
     ));
     $this->dbforge->add_key('id', TRUE);
     $this->dbforge->create_table('pedidos');
-    $this->dbforge->add_column('pedidos',[
-        'CONSTRAINT pedidos_clientes_id_fk FOREIGN KEY (id_cliente) REFERENCES clientes (id) ON DELETE SET NULL',
-    ]);
+
+    $this->dbforge->add_field(array(
+        'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE,
+                'auto_increment' => TRUE
+        ),
+        'id_pedido' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE
+        ),
+        'nome_item' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'quantidade' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'valor_unitario' => array(
+                'type' => 'DECIMAL',
+                'constraint' => '10,2'
+        ),
+        'valor_total' => array(
+                'type' => 'DECIMAL',
+                'constraint' => '10,2'
+        ),
+        'valor_total_item' => array(
+                'type' => 'DECIMAL',
+                'constraint' => '10,2'
+        ),
+        'CONSTRAINT `pedidos_item_pedidos_id_fk` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`) ON DELETE CASCADE'
+    ));
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('pedidos_item');
+
+    $this->dbforge->add_field(array(
+        'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE,
+                'auto_increment' => TRUE
+        ),
+        'id_marca' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE
+        ),
+        'id_categoria' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE
+        ),
+        'nome' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'cod_produto' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'destaque' => array(
+                'type' => 'TINYINT',
+                'constraint' => 1
+        ),
+        'ativo' => array(
+                'type' => 'TINYINT',
+                'constraint' => 1
+        ),
+        'controlar_estoque' => array(
+                'type' => 'TINYINT',
+                'constraint' => 1
+        ),
+        'estoque' => array(
+                'type' => 'INT',
+                'constraint' => 11
+        ),
+        'data_cadastro' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'ultima_atualizacao' => array(
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+                'null' => TRUE
+        ),
+        'peso' => array(
+                'type' => 'INT',
+                'constraint' => 11
+        ),
+        'ativo' => array(
+                'type' => 'INT',
+                'constraint' => 11
+        ),
+        'altura' => array(
+                'type' => 'INT',
+                'constraint' => 11
+        ),
+        'largura' => array(
+                'type' => 'INT',
+                'constraint' => 11
+        ),
+        'comprimento' => array(
+                'type' => 'INT',
+                'constraint' => 11
+        ),
+        'info' => array(
+                'type' => 'longtext'
+        ),
+        'meta_link' => array(
+                'type' => 'DECIMAL',
+                'constraint' => '10,2'
+        ),
+        'CONSTRAINT `produtos_categorias_id_fk` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`)',
+        'CONSTRAINT `produtos_marcas_id_fk` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`)',
+    ));
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('produtos');
+
+    $this->dbforge->add_field(array(
+        'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE,
+                'auto_increment' => TRUE
+        ),
+        'id_produto' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE
+        ),
+        'foto' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'CONSTRAINT `fotos_produtos_id_fk` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`)'
+    ));
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('produtos_fotos');
+
+    $this->dbforge->add_field(array(
+        'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE,
+                'auto_increment' => TRUE
+        ),
+        'id_produto' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => FALSE
+        ),
+        'quant_vendidos' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+        ),
+        'CONSTRAINT `FK_produtos_mais_vendidos` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`) ON DELETE CASCADE'
+    ));
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('produtos_mais_vendidos');
+
     }
 }
