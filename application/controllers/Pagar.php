1906fb69458b8267e9d7a35becb7797574f63618
Rafinhaa <rafinhaa@me.com>
2021-04-22 15:26:39 -0300

    5. api pagseguro editada

--- application/controllers/Pagar.php
+++ application/controllers/Pagar.php
@@ -23,12 +23,15 @@ class Pagar	extends CI_Controller {
 		$ch = curl_init();
 
 		if($query->sandbox == 1){
-			$url = 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions';
-			curl_setopt($ch,CURLOPT_SSL_VERIFYPPER,false);
+			$url = 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions?';
+			//curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
 		}else{
-			$url = 'https://ws.pagseguro.uol.com.br/v2/sessions';
-			curl_setopt($ch,CURLOPT_SSL_VERIFYPPER,true);
+			$url = 'https://ws.pagseguro.uol.com.br/v2/sessions?';
+			//curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,true);
 		}		
+
+		//curl_setopt($ch,CURL_HTTPHEADER,array('Content-Type: application/x-www-form-urlencoded; charset=ISO-UTF-8'));
+
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_POST,2);
 		curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($param));
@@ -36,10 +39,13 @@ class Pagar	extends CI_Controller {
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
 
+		//echo $url . http_build_query($param); die;
+
 		$result = curl_exec($ch);
 		curl_close($ch);
 
-		print_r($result);
+		$xml = simplexml_load_file($result);
 
+		echo json_encode($xml);
 	}
 }
